7c24d55d119ee4077912455e16e8da379e63ffef
Tbolivar <thomassvpbolivar@gmail.com>
2019-04-21 20:41:09 +0400

    Added:  Simple Unit test

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -4,15 +4,21 @@ class BankAccountTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-
+    BankAccount bankAccount = new BankAccount("Thomas","Bolivar",1000.00);
+    double balance = bankAccount.deposit(150);
+    assertEquals(1150,balance,0);
 
     }
 
     @org.junit.jupiter.api.Test
     void withdraw() {
+        fail("This test is not yet implemented");
     }
 
     @org.junit.jupiter.api.Test
     void getBalance() {
+        fail("This test is not yet implemented");
     }
+
+
 }
